e49ee8d4c8c097b06ee6c0220f552bc38846273f
jannahuang <38380050+jannahuang@users.noreply.github.com>
2019-01-17 12:40:34 +0800

    Update CaesarCipher.js

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -1,29 +1,57 @@
-// Caesar cipher 凯撒加密
-// 对于一个字符串整体移位，将明文加密成密码
+// Caesar Cipher 凯瑟加密算法的暴力解密方法
+
 var lower = 'abcdefghijklmnopqrstuvwxyz'
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-var encode = function(s) {
-    var result = ""
-    var len = s.length
+// 封装输出语句为 log 函数
+var log = function() {
+    console.log.apply(console, arguments)
+}
+
+// 将大写字母统一成小写字母，便于阅读
+var getLowercase = function(letter) {
+    for (var i = 0; i < upper.length; i++) {
+        if (letter == upper[i]) {
+            return lower[i]
+        }
+    }
+}
+
+// 将字母移位
+var getShift = function(shift, letter) {
+    for (var i = 0; i < lower.length; i++) {
+        if (letter == lower[i]) {
+            var index = i
+        }
+    }
+    if (index + shift >= 26) {
+        return lower[index + shift - 26]
+    }else{
+        return lower[index + shift]
+    }
+}
+
+// 主函数
+var CaesarCipher = function(string, shift) {
+    result = ''
+    len = string.length
     for (var i = 0; i < len; i++) {
-        var sl = s[i]
-        var index = find(lower,s[i])
-        if (s[i] == 'z') {
-            result += lower[0]
+        if (upper.includes(string[i])){
+            result += getShift(shift, getLowercase(string[i]))
+        }else if (lower.includes(string[i])) {
+            result += getShift(shift, string[i])
         }else {
-            result += lower[index+1]
+            result += string[i]
         }
     }
     return result
 }
 
-var find = function(lower, x) {
-    for (var i = 0; i < lower.length; i++) {
-        if (x == lower[i]) {
-            return i
-        }
-    }
-}
+// 将密文放置此处
+code = 'VRPHWLPHV L ZDQW WR FKDW ZLWK BRX, \
+EXW L KDYH QR UHDVRQ WR FKDW ZLWK BRX'
 
-encode(abcefg)
+// 循环 25 次暴力破解密文
+for (var i = 1; i < 26; i++) {
+    log(i, CaesarCipher(code, i))
+}
